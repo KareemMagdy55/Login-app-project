079737afaa298087662606550f6eb30677674cb3
KareemMagdy55 <91559979+KareemMagdy55@users.noreply.github.com>
2022-05-15 20:40:21 +0200

    Password conditions function added.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+using namespace std ;
+
+void passwordConditions();
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
+
+void passwordConditions(){
+    cout << "Password must require at least one of the following:\n"
+            "\n"
+            "1.Uppercase and lowercase letter (A, z)\n"
+            "2.Numeric character (0-9)\n"
+            "3.Special character (any character your environment will accept that is not an uppercase or a"
+            " lowercase letter or a numeric character, for example ( !, %, @, #, and so on).\n";
+}
+
+
+
